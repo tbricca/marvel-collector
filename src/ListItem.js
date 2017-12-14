5e84d11cc9c77873f90b24d4ac5bfa91691f9b4a
tbricca <tbricca@gmail.com>
2017-12-13 21:10:36 -0800

    added additional tags for the api call

--- src/ListItem.js
+++ src/ListItem.js
@@ -4,11 +4,15 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
+      // lookup exactly how to properly use metadata
       const metaData =this.props.comic;
     const {thumbnail} = comic.data.results[0] 
     return (
       <div className="App">
       <h1>{comic.data.results[0].title}</h1> 
+      <h1>Issue</h1>
+      <h1>Date</h1>
+      <h1>Price</h1>
       <img className="comic-thumbnail" src= {`${thumbnail.path}.${comic.data.results[0].thumbnail.extension}`} />
       </div>
     );
